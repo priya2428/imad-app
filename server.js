cd2b90b047b0cd16cef18da1fae3b8b89121dc53
priya2428 <34018725+priya2428@users.noreply.github.com>
2017-12-04 15:29:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-app.get('/one',function(req,res){
-    res.send('article is being served');} )
+app.get('/article-one',function(req,res){
+    res.sendFile(path.join(__dirname,'ui','article-one.html');} )
 app.get('/two',function(req,res){
     res.send('article is being served');} )
 
