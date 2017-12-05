d1ff7983671e72bd5d5adac0f47afc562d6aae24
priya2428 <34018725+priya2428@users.noreply.github.com>
2017-12-05 16:48:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,8 +4,13 @@ console.log('absd');
 var element=document.getElementById('main-text');
 element.innerHTML ='new value';
 var img= document.getElementById('madi');
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft+10;
+    img.style.marginLeft+'px';
+}
 img.onclick= function(){
-img.style.marginLeft='100px';
+var interval=setInterval(moveRight,100);
 };
 
 
