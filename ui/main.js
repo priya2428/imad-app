829be5dee48fb4ae2efbbf5f7dbf6b76d1ab4862
priya2428 <34018725+priya2428@users.noreply.github.com>
2017-12-06 18:46:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,10 @@
 var button =document.getElementById('counter');
+var counter=0;
 button.onclick=function(){
-    var request= new XMLHttpRequest();
-    request.onreadystatechange =function(){
-        if(request.readystatus===XMLHttpRequest.DONE){
-            if(request.status===200){
-                var counter=request.responseText;
-                var span=document.getElementById('count');
-                span.innerHTML=counter.toString();
-                
-            }
-        }
-    };
-    request.open('GET','http://priyadharshni2palani.imad.hasura-app.io/counter',true);
-    request.send(null);
+    counter=counter+1;
+    var span=document.getElementById('count');
+    span.innerHTML=counter.toString();
 };
 
+    
 
