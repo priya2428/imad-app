93c6acbbb3d7eedba625fac6aeedfa1c83af713e
priya2428 <34018725+priya2428@users.noreply.github.com>
2017-12-06 18:05:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,9 @@ var request= new xmlHttpRequest();
 request.onreadystatechange=function(){
 if (request.readystate===xmlHttpRequest.DONE){
     if(request.status===200){
-        request.responseText;
+       var counter= request.responseText;
+       var span=document.getElementById('count');
+       span.innerHTML=counter.toString();
     }
 }
 
