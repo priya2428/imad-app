f415120046bea6ce3fd8886be35309bc57eb6523
priya2428 <34018725+priya2428@users.noreply.github.com>
2017-12-06 11:21:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,4 @@
-console.log('loaded');
-var img= document.getElementById('madi');
-var marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft+1;
-    img.style.marginLeft= marginLeft+'px';
-}
-img.onclick= function(){
-var interval=setInterval(moveRight,50);
-};
+
 var button =document.getElementById('counter');
 button.onclick=function(){
     counter=counter+1;
